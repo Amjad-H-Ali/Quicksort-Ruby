de320adc06abca89201ddd58581694dcb01bf568
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-18 09:05:20 -0500

    Recursively do the above code to the sub arrays, while placin pivot in between; Changed insert() to push()

--- qs.rb
+++ qs.rb
@@ -23,17 +23,19 @@ def quick_sort array
 	for i in 0...arr_Len - 1
 		# When number less than pivot, place in left
 		if array[i] < pivot
-			left.insert(aray[i])
+			left.push(array[i])
 
 		# If not, place in right	
 		else
-			right.insert(array[i])
+			right.push(array[i])
 		end	
 		 	 
-	end	 
+	end	
 
+	# Recursively do the above code to the sub arrays, while placin pivot in between
+	[*quick_sort(left), pivot, *quick_sort(right)] 
 
 end		
 
 
-quick_sort [75, 9, 45, 3, 0, 1, 10, 1, 1, 32, 12, 7]
+p quick_sort [75, 9, 45, 3, 0, 1, 10, 1, 1, 32, 12, 7]
