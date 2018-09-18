cac632aa17f9dfa78cde3e14e37522afc349d5e3
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-18 08:48:52 -0500

    Choose last element array to be pivot

--- qs.rb
+++ qs.rb
@@ -5,9 +5,13 @@ def quick_sort array
 	# Declared variable and initialized with array length
 	arr_Len = array.length
 
+	# Exit code for recursion
 	if arr_Len < 2
 		return array
-	end	
+	end
+
+	# Choose last element array to be pivot
+	pivot = array[arr_Len - 1]	
 
 end		
 
