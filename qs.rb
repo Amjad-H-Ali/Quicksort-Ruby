00c29872ba00bdae13953e8bd564dd1600577401
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-18 08:46:05 -0500

    Declared variable and initialized with array length

--- qs.rb
+++ qs.rb
@@ -2,5 +2,9 @@ p "Quicksort!"
 
 # Define a function that takes in array and returns it
 def quick_sort array
-	array
+	arr_Len = array.length
+
 end		
+
+
+quick_sort [75, 9, 45, 3, 0, 1, 10, 1, 1, 32, 12, 7]
